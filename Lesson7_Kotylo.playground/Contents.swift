447423a22d7d4bbd72b8c1761e9840a78538245a
Kotylo <kirillkotylo@gmail.com>
2025-03-13 23:23:11 +0300

    Create class shape

--- Lesson7_Kotylo.playground/Contents.swift
+++ Lesson7_Kotylo.playground/Contents.swift
@@ -7,4 +7,15 @@ import UIKit
 //•  Переопределите метод calculateArea() в каждом подклассе для вычисления площади соответствующей фигуры.
 //•  Добавьте свойства, специфичные для каждой фигуры (например, радиус для круга, длина и ширина для прямоугольника).
 //•  Создайте массив объектов типа Shape и выведите площадь каждой фигуры.
+class Shape {
+    var color: String
+    
+    init(color: String) {
+        self.color = color
+    }
+    
+    func calculateArea() -> Double {
+       fatalError("Subclasses must implement calculateArea()")
+    }
+}
 
