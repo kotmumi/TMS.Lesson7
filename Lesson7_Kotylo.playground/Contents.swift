ac54e94d1d4f95d9502ea3c39493d51d2b424d46
Kotylo <kirillkotylo@gmail.com>
2025-03-15 21:26:13 +0300

    Create stuct Contact and add func search

--- Lesson7_Kotylo.playground/Contents.swift
+++ Lesson7_Kotylo.playground/Contents.swift
@@ -81,6 +81,8 @@ final class Triangle: Shape {
     }
 }
 
+// MARK: -Test
+
 func randomData () -> Double { Double.random(in: 0...10) }
 
 var shapes: [Shape] = []
@@ -91,3 +93,48 @@ shapes.append(Rectangle(height: randomData(), side: randomData(), color: "green"
 shapes.append(Triangle(base: randomData(), height: randomData(), color: "red"))
 
 shapes.forEach { $0.printArea() }
+
+
+// MARK: -2. Создание структуры для представления контакта в телефонной книге:
+
+ // •  Создайте структуру Contact со свойствами:
+ //   *  firstName (String)
+ //   *  lastName (String)
+ //   *  phoneNumber (String)
+ //   *  email (String?) (опциональный email).
+ // •  Создайте функцию, которая принимает массив структур Contact и строку для поиска. Функция должна возвращать новый массив Contact, содержащий только те контакты, у которых имя или фамилия содержат строку для поиска (без учета регистра).
+
+struct Contact {
+    let firstName: String
+    let lastName: String
+    var fullName: String {
+        "\(firstName) \(lastName)"
+    }
+    let phoneNumber: String
+    var email: String? {
+        "\(firstName)\(lastName)@gmail.com"
+    }
+    
+    static func search(name: String, phoneDirectory: [Contact]) -> [Contact] {
+        phoneDirectory.filter { $0.fullName.lowercased().contains(name.lowercased()) }
+    }
+}
+
+//MARK: -Test
+
+var phoneDirectory: [Contact] = []
+
+phoneDirectory.append(Contact(firstName: "Kirill", lastName: "Kotylo", phoneNumber: "+375330000001"))
+phoneDirectory.append(Contact(firstName: "Dmitriy", lastName: "Kotov", phoneNumber: "+375330000002"))
+phoneDirectory.append(Contact(firstName: "Vlad", lastName: "Ivanov", phoneNumber: "+375330000003"))
+phoneDirectory.append(Contact(firstName: "Egor", lastName: "Makarov", phoneNumber: "+375330000004"))
+phoneDirectory.append(Contact(firstName: "Vasiliy", lastName: "Petrov", phoneNumber: "+375330000005"))
+phoneDirectory.append(Contact(firstName: "Evgeniy", lastName: "Petrov", phoneNumber: "+375330000006"))
+phoneDirectory.append(Contact(firstName: "", lastName: "", phoneNumber: "+375330000007"))
+
+print("\nSearch result for name \"oV\":")
+Contact.search(name: "oV", phoneDirectory: phoneDirectory).forEach { print("\($0.fullName) - \($0.phoneNumber) - \($0.email ?? "No email")") }
+print("\nSearch result for name \"KOT\":")
+Contact.search(name: "KOT", phoneDirectory: phoneDirectory).forEach { print("\($0.fullName) - \($0.phoneNumber) - \($0.email ?? "No email")") }
+print("\nSearch result for name \"EG\":")
+Contact.search(name: "EG", phoneDirectory: phoneDirectory).forEach { print("\($0.fullName) - \($0.phoneNumber) - \($0.email ?? "No email")") }
